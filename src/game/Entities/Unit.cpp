f1bf0d28c4c31aeaa1e97333a9649ebad1fec13d
celguar <celguar@gmail.com>
2021-06-22 03:34:44 +0300

    Crashfixes
    Fix crash in player relocation
    Fix crash in targeted movement generator log
    Crashfix attempt
    Temp fix for bg event crash
    BG crashfix
    
    Co-Authored-By: blitzi <289959+blitzi@users.noreply.github.com>

--- src/game/Entities/Unit.cpp
+++ src/game/Entities/Unit.cpp
@@ -6451,6 +6451,7 @@ void Unit::RemoveNotOwnTrackedTargetAuras(uint32 newPhase)
 
 void Unit::RemoveSpellAuraHolder(SpellAuraHolder* holder, AuraRemoveMode mode)
 {
+    MANGOS_ASSERT(holder);
     MANGOS_ASSERT(!holder->IsDeleted());
 
     // Statue unsummoned at holder remove
@@ -6535,7 +6536,8 @@ void Unit::RemoveAura(Aura* Aur, AuraRemoveMode mode)
     // remove from list before mods removing (prevent cyclic calls, mods added before including to aura list - use reverse order)
     if (Aur->GetModifier()->m_auraname < TOTAL_AURAS)
     {
-        m_modAuras[Aur->GetModifier()->m_auraname].remove(Aur);
+        if (std::find(m_modAuras[Aur->GetModifier()->m_auraname].begin(), m_modAuras[Aur->GetModifier()->m_auraname].end(), Aur) != m_modAuras[Aur->GetModifier()->m_auraname].end())
+            m_modAuras[Aur->GetModifier()->m_auraname].remove(Aur);
     }
 
     // Set remove mode
@@ -8717,7 +8719,7 @@ uint32 Unit::SpellDamageBonusTaken(Unit* caster, SpellSchoolMask schoolMask, Spe
     AuraList const& mOwnerTaken = GetAurasByType(SPELL_AURA_MOD_DAMAGE_FROM_CASTER);
     for (auto i : mOwnerTaken)
     {
-        if (i->GetCasterGuid() == caster->GetObjectGuid() && i->isAffectedOnSpell(spellProto))
+        if (caster && i->GetCasterGuid() == caster->GetObjectGuid() && i->isAffectedOnSpell(spellProto))
             TakenTotalMod *= (i->GetModifier()->m_amount + 100.0f) / 100.0f;
     }
 
