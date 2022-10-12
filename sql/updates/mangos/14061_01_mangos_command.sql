218fd3ad9b9655fb6839486abb8478843eab56a4
Adam Semper <kadeshar@gmail.com>
2022-10-12 21:10:57 +0200

    Added new playerbot commands on website

---
+++ sql/updates/mangos/14061_01_mangos_command.sql
@@ -0,0 +1,7 @@
+ALTER TABLE db_version CHANGE COLUMN required_14060_01_mangos_waypoint_path_name required_14061_01_mangos_command bit;
+
+DELETE FROM command WHERE name IN ('bot add', 'bot self');
+
+INSERT INTO `command`(`name`, `security`, `help`) VALUES
+('bot add',0,'Syntax: .bot add [$botname]\r\nAdd bot to world.'),
+('bot self',0,'Syntax: .bot self\r\nEnable or disable playerbot AI on current logged player. Client side only.');
