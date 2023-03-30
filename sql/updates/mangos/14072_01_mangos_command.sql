600b63c2e77d7214f157e420a7b835fcca0039b1
Adam Semper <kadeshar@gmail.com>
2023-03-30 15:58:16 +0200

    Reverted db version change

--- sql/updates/mangos/14072_01_mangos_command.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE db_version CHANGE COLUMN required_14071_01_mangos_spawn_data_equip required_14072_01_mangos_command bit;
-
-DELETE FROM command WHERE name IN ('bot add', 'bot self');
-
-INSERT INTO `command`(`name`, `security`, `help`) VALUES
-('bot add',0,'Syntax: .bot add [$botname]\r\nAdd bot to world.'),
-('bot self',0,'Syntax: .bot self\r\nEnable or disable playerbot AI on current logged player. Client side only.');
